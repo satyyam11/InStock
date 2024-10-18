292285b2b010ada434a54b1079f636ee81b1a6d5
Satyam Mishra <satyyam.misshra1121@gmail.com>
2024-10-19 03:56:07 +0530

    Updated routes/inventory.js

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { getInventory, createItem, updateItem, deleteItem } = require('../controllers/inventoryController');
-const authenticateUser = require('../config/authenticateUser');
+const { getInventory, createItem, updateItem, deleteItem, importCSV, exportCSV } = require('../controllers/inventoryController');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const authenticateUser = require('../config/authenticateUser'); 
 
-router.use(authenticateUser);
-router.get('/', getInventory);
-router.post('/', createItem);
-router.put('/:id', updateItem);
-router.delete('/:id', deleteItem);
+// Protect routes with authenticateUser middleware
+router.get('/', authenticateUser, getInventory);
+router.post('/', authenticateUser, createItem);
+router.put('/:id', authenticateUser, updateItem);
+router.delete('/:id', authenticateUser, deleteItem);
+router.post('/import-csv', authenticateUser, upload.single('file'), importCSV);
+router.get('/export-csv', authenticateUser, exportCSV);
 
 module.exports = router;
