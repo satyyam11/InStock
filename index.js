df9b804390087c574f8638589d28dd95a247efc8
Satyam Mishra <satyyam.misshra1121@gmail.com>
2024-10-24 01:45:34 +0530

    added logout and change password feature

--- index.js
+++ index.js
@@ -9,15 +9,21 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const JWT_SECRET = process.env.JWT_SECRET;
 
+// Middleware
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: JWT_SECRET, resave: false, saveUninitialized: true }));
+app.use(session({ 
+    secret: JWT_SECRET, 
+    resave: false, 
+    saveUninitialized: true 
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Routes
 app.use('/auth', authRoutes);
 app.use('/inventory', inventoryRoutes);
+app.get('/check', (req, res) => res.send('InStock backend is running!'));
 
-app.get('/', (req, res) => res.send('InStock backend is running!'));
-
+// Start the server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
